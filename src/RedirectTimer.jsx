19d0d22fbeca82f3dabbe186070cba414ed61b6e
riva211 <rivamakhani1@gmail.com>
2025-01-30 22:53:09 +0530

    figma uploaded

--- src/RedirectTimer.jsx
+++ src/RedirectTimer.jsx
@@ -14,14 +14,14 @@ const RedirectTimer = () => {
       return () => clearInterval(timer);
     } else {
       // Redirect when countdown reaches 0
-      window.location.href = 'https://bhaikishaadi.vercel.app/';
+      window.location.href = 'https://www.figma.com/proto/2iK5aqtj1nUFcJt7rPE4nf/Untitled?node-id=1-45&p=f&t=nSRJsd47uZELtMlk-1&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=1%3A45';
     }
   }, [countdown]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="p-8 bg-white rounded-lg shadow-md">
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+    <div className="flex flex-col items-center justify-center min-h-screen ">
+      <div className="p-8 bg-white rounded-lg shadow-md flex  ">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">                       
           Redirecting to Tara...
         </h1>
         <p className="text-xl text-gray-600">
